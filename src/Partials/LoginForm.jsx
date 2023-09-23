ab65983e96149a327dbaf4d4e14ee8df403da4c5
navneetNFS <navneettaneja.fullstack@gmail.com>
2023-09-23 10:25:54 +0530

    login functionality

--- src/Partials/LoginForm.jsx
+++ src/Partials/LoginForm.jsx
@@ -1,16 +1,75 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function LoginForm() {
+
+    const initialValue = {
+        email : '',
+        password: ''
+    }
+
+    const [formData,setFormData] = useState(initialValue);
+    const {email,password} = formData;
+
+    const [error,setError] = useState({})
+
+    const validation = () => {
+        if(!email && !password){
+            const err = {}
+            err.email = "Email is required field"
+            err.password = "Password is required field"
+            setError(err)
+            return err
+        }
+        else if(!email){
+            const err = {}
+            err.email = "Email is required field"
+            setError(err)
+            return err
+        }
+        else if (!password){
+            const err = {}
+            err.password = "Password is required field"
+            setError(err)
+            return err
+        }
+        else{
+            setError({})
+            return {}
+        }
+    }
+
+    const loginNow = ({email,password}) => {
+        console.log(`email is ${email} , password is ${password}`);
+        // window.location.reload(true)
+    }
+
+    const onHandelSubmit = (e) => {
+        e.preventDefault();
+        const isValid = validation();
+        const isValidKeys = Object.keys(isValid)
+        if(isValidKeys.length  == 0){
+            loginNow(formData)
+        }
+    }
+
+    const onHandelChange = (e) => {
+        const {name,value} = e.target
+        setFormData({...formData,[name]:value})
+    }
+
     return (
         <>
             <h1>Sign In</h1>
-            <form action="" method="POST">
+            <form method="POST" onSubmit={onHandelSubmit}>
                 <div className="form-group mb-5">
                     <div className="form-field mb-3">
-                        <input type="email" name="email" id="userEmail" className="form-control" placeholder="Email" />
+                        <input type="email" name="email" id="userEmail" className="form-control" placeholder="Email" value={email} onChange={onHandelChange} />
+                        <span>{error.email}</span>
                     </div>
                     <div className="form-field mb-3">
-                        <input type="text" name="password" id="userPwd" className="form-control" placeholder="Password" />
+                        <input type="text" name="password" id="userPwd" className="form-control" placeholder="Password" value={password} onChange={onHandelChange} />
+                        <span>{error.password}</span>
                     </div>
                     <div className="form-field mb-3 text-end">
                         <a href="/" className="text-primary">Forgot Password</a>
